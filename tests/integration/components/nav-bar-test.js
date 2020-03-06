c91c6bec6888ba25d4fc61573f50e5db334da850
Jacob Ela <jacob.ela@gmail.com>
2020-03-06 14:38:27 -0600

    Update tests to fix failures
    These aren't super-effective tests, but they weren't much more than this
    in the pre-updated code either.

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -12,15 +12,6 @@ module('Integration | Component | nav-bar', function(hooks) {
 
     await render(hbs`<NavBar />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <NavBar>
-        template block text
-      </NavBar>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(this.element);
   });
 });
